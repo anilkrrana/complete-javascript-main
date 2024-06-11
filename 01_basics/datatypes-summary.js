99006d4f27ec118734dbb538270c1a8499051db0
Anil Kumar Rana <anilkrana474@gmail.com>
2024-06-11 12:19:35 +0530

    Memories in JS

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,3 +34,34 @@ const myFunction = function(){
 console.log(typeof scoreValue);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+// Memories
+
+// Stack (Primitive), Heap (Non-Premitive)
+
+let myName = "Anil"
+
+let anotherName = myName
+anotherName = "Rahul"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = " anil@gmail.com"
+
+
+console.log(userOne.email);
+console.log(userTwo.email);
+ 
+
