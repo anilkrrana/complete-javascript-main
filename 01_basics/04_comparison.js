416f9107ce39b8d1f2f2e30981fe2d22974ee4b0
Anil Kumar Rana <anilkrana474@gmail.com>
2024-06-11 11:43:02 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+
+// this is too easy but when we dont compare same things in js;
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// Avoide these type of conversions
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// ===
+
+console.log("2" === 2);
